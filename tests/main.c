775932fe790cef4e3a2c155b2000eeee1c8f8e45
sangwani <mightypz@gmail.com>
2021-11-01 09:44:03 +0200

    Second commit

--- tests/main.c
+++ tests/main.c
@@ -1,69 +1,14 @@
-#include "shell.h"
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-
-/**
- *main - start a new process
- *
- *Return: Always 0
- */
-int main(__attribute__((unused)) int argc, __attribute__((unused))char *argv[])
+ 
+int call_cmd(char *prog, char **args)
 {
-	char *str;
-       	ssize_t chars = 0;
-	size_t buf_size;
-	
-	while (1)
-	{
-		write(1, "$ ", 2);
-		chars = getline(&str, &buf_size, stdin);
-
-		if (*str == '\0')
-			continue;
-		if (chars < 0)
-			exit(1);
-		else
-		{
-			exec_command(str);
-		}
-	}
-	free(str);
-	str = NULL;
-	return(0);
+	execve(prog, args, NULL);
+	perror("./shell");
 }
 
-/**
- *exec_cmd - Creates a new child process
- *executes a command, and waits for the child process.
- *@cmd command
- *@argv: vector of pointers to commands
- *
- *Return: void
- */
-void exec_command(char *cmd)
+int main()
 {
-	pid_t pid;
-	int status;
-	char *argv[3];
-
-	argv[0] = "/bin/";
-	argv[1] = cmd;
-	argv[2] = NULL;
-
-	pid = fork();
-	if (pid < 0)
-		perror("/bin/");
-	else if (pid == 0)
-	{
-		execve(argv[0], argv, NULL);
-		perror("./shell");
-		exit(1);
-	}
-	else
-	{
-		wait(&status);
-	}
+	char *args[] = {"/bin/sh", NULL};
+	call_cmd(args[0], args);
 }
--- tests/shell.h
+++ tests/shell.h
@@ -1,6 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-void exec_command(char *cmd);
+void exec_command(char *cmd, char *argv, char *env);
 
 #endif
